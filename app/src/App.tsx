02377683cef41ad35d37eebd7b17fe5b06ef0bfe
gabbwhxz <vnvsgabe@gmail.com>
2024-11-08 12:44:57 -0300

    add future no router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 
+import GlobalStyle from './GlobalStyle'
+
 import AppRouter from './routes/routes'
 
 export default function App() {
   return (
-    <Router>
-      <AppRouter />
-    </Router>
+    <>
+      <Router future={{ v7_startTransition: true }}>
+        <AppRouter />
+      </Router>
+      
+      <GlobalStyle />
+    </>
   )
 }
