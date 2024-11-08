2491f00ad78874226c60a4fc863cdec782a92c93
gabbwhxz <vnvsgabe@gmail.com>
2024-11-08 12:45:33 -0300

    criado header

---
+++ app/src/components/layout/Header.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  background-color: #121212;
+  color: white;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+`
+
+const Title = styled.p`
+  font-size: 30px;
+  font-weight: 500;
+  margin: 0;
+`
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <Title>PC Info & Eletro</Title>
+    </HeaderContainer>
+  )
+}
