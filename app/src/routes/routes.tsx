ef0c07d7db255b89ada5bd9e8416848fb453f467
gabbwhxz <vnvsgabe@gmail.com>
2024-11-09 22:56:27 -0300

    adicionado novas rotas

--- app/src/routes/routes.tsx
+++ app/src/routes/routes.tsx
@@ -3,12 +3,16 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from '../components/layout/Layout'
 
 import FechamentoMes from '../pages/fechamento/FechamentoMes'
+import Agenda from '../pages/agenda/Agenda'
+import Registro from '../pages/registro/Registro'
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/fechamento" element={<FechamentoMes />} />
+        <Route path="/agenda" element={<Agenda />} />
+        <Route path="/registro" element={<Registro />} />
       </Route>
     </Routes>
   )
