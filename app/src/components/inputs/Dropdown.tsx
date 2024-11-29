67c33325060dcc820127e994bc748959d8195832
gabbwhxz <vnvsgabe@gmail.com>
2024-11-29 17:17:45 -0300

    ajustado props do dropdown

--- app/src/components/inputs/Dropdown.tsx
+++ app/src/components/inputs/Dropdown.tsx
@@ -3,17 +3,26 @@
 import { Controller, Control, FieldValues } from 'react-hook-form'
 import styled from 'styled-components'
 
-const StyledSelect = styled.select<{ $width?: string; $marginRight?: string }>`
+const StyledSelect = styled.select<{
+  $width?: string
+  $marginRight?: string
+  $disabled?: boolean
+}>`
   width: ${(props) => props.$width || '100%'};
   height: 30px;
-  background-color: transparent;
-  border: 1px solid #222222;
+  background-color: ${(props) => (props.$disabled ? '#f0f0f0' : 'transparent')};
+  border: 1px solid ${(props) => (props.$disabled ? '#ccc' : '#222222')};
   border-radius: 5px;
   padding: 5px;
   font-size: 13px;
-  color: black;
+  color: ${(props) => (props.$disabled ? '#999' : 'black')};
   margin-right: ${(props) => props.$marginRight || '0px'};
   outline: none;
+  cursor: ${(props) => (props.$disabled ? 'not-allowed' : 'text')};
+
+  option {
+    color: ${(props) => (props.$disabled ? '#ccc' : 'black')};
+  }
 `
 
 interface DropdownProps {
@@ -26,6 +35,7 @@ interface DropdownProps {
   placeholder?: string
   id: string
   onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void
+  $disabled?: boolean // Nova propriedade disabled
 }
 
 export default function Dropdown({
@@ -38,6 +48,7 @@ export default function Dropdown({
   placeholder,
   id,
   onChange,
+  $disabled, // Valor padrão do disabled
 }: DropdownProps) {
   return (
     <Controller
@@ -50,12 +61,14 @@ export default function Dropdown({
           $width={width}
           $marginRight={marginRight}
           id={id}
+          $disabled={$disabled} // Passando a propriedade disabled para o styled component
           onChange={(e) => {
             field.onChange(e) // update react-hook-form state
             if (onChange) {
               onChange(e)
             }
           }}
+          disabled={$disabled} // Aqui o atributo 'disabled' está sendo aplicado ao select
         >
           {placeholder && <option value="">{placeholder}</option>}
           {options.map((option) => (
