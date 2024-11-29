aee4a0e20df33350603ef361c43c9b757c47b8f1
gabbwhxz <vnvsgabe@gmail.com>
2024-11-29 17:19:27 -0300

    add soma total

--- api/src/registro/registro.service.ts
+++ api/src/registro/registro.service.ts
@@ -31,6 +31,23 @@ export class RegistroService {
     return registro;
   }
 
+  async getFechamentoMes(mes: number, ano: number) {
+    const registros = await this.registroRepository
+      .createQueryBuilder('registro')
+      .select([
+        'registro.formaPagamento',
+        'registro.contaAplicada',
+        'SUM(registro.valorPago) AS totalPago', // soma total pago por forma de pagamento e conta
+        'SUM(CASE WHEN registro.deve = true THEN registro.valorDevido ELSE 0 END) AS totalDevido', // soma o total a receber
+      ])
+      .where('MONTH(registro.data) = :mes', { mes })
+      .andWhere('YEAR(registro.data) = :ano', { ano })
+      .groupBy('registro.formaPagamento, registro.contaAplicada') // agrupa por forma de pagamento e conta
+      .getRawMany();
+
+    return registros;
+  }
+
   async remove(id: number): Promise<void> {
     await this.registroRepository.delete(id);
   }
