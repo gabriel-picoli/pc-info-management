09f1dfd6392749c0990c567e077a5ca83aecfb45
gabbwhxz <vnvsgabe@gmail.com>
2024-11-29 17:18:40 -0300

    alterado url

--- app/src/axios-config.ts
+++ app/src/axios-config.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios'
 
 // configura a url base da api e o tempo limite de requisiçoes
 const api: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://localhost:3306',
   timeout: 5000, // tempo limite em milissegundos
   headers: {
     'Content-Type': 'application/json', // tipo de conteudo padrao
